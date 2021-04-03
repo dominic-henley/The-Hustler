f48e8b12c6ede0bded37e3f106a36d424c260afe
dominichenley986 <dhenley@student.unimelb.edu.au>
2021-04-03 16:01:31 +0700

    testing stuff

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('homepage.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 
