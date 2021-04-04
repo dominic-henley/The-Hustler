b2da5ffe28c406478cae68d190da95332d6d8024
dominic-henley <80395298+dominic-henley@users.noreply.github.com>
2021-04-04 17:29:53 +0700

    added login stuff

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '3f3040542f9e39e5cc0a30ac826f383d'
 
 posts = [
     {
@@ -27,15 +30,22 @@ def home():
 def about():
     return render_template('about.html', title="About Us")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template('register.html', title="register")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', "success")
+        return redirect(url_for('home'))
+    return render_template('register.html', title="register", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title="login")
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'logged in with {form.email.data}', "success")
+        return redirect(url_for('home'))
+    return render_template('login.html', title="login", form=form)
 
 @app.route("/progression")
 def test():
