a65dfde2489217db01e5e683d280235dfe20456e
dominic-henley <80395298+dominic-henley@users.noreply.github.com>
2021-04-04 18:46:02 +0700

    changing imports

--- package/__init__.py
+++ package/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '3f3040542f9e39e5cc0a30ac826f383d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///journal.db'
-
 db = SQLAlchemy(app)
+
+from package import routes
+
