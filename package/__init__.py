7585848b38af88a84407e334368a682389b727cf
dominic-henley <80395298+dominic-henley@users.noreply.github.com>
2021-04-04 20:06:02 +0700

    finished login

--- package/__init__.py
+++ package/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3f3040542f9e39e5cc0a30ac826f383d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///journal.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from package import routes
 
